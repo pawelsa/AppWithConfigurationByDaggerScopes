8a44b6e30581f7ae62398b31dd6146635e3ed2c6
psapekinfotower <psapek@infotower.pl>
2025-03-09 22:14:56 +0100

    Updated dependencies, Readme.md, moved RepositoryEntryPointsBridgeModule to separate file

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,12 +7,12 @@ plugins {
 
 android {
     namespace = "com.example.appwithconfigurationbydaggerscopes"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.appwithconfigurationbydaggerscopes"
         minSdk = 26
-        targetSdk = 33
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
@@ -56,27 +56,27 @@ kapt {
 }
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.13.1")
-    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("androidx.core:core-ktx:1.15.0")
+    implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.0")
-    implementation("androidx.activity:activity-compose:1.9.0")
-    implementation(platform("androidx.compose:compose-bom:2024.04.01"))
+    implementation("androidx.constraintlayout:constraintlayout:2.2.1")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
+    implementation("androidx.activity:activity-compose:1.10.1")
+    implementation(platform("androidx.compose:compose-bom:2025.02.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
     testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation("androidx.test.ext:junit:1.2.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
 
-    val navVersion = "2.7.7"
+    val navVersion = "2.8.8"
     implementation("androidx.navigation:navigation-fragment-ktx:$navVersion")
     implementation("androidx.navigation:navigation-ui-ktx:$navVersion")
     implementation("androidx.navigation:navigation-compose:$navVersion")
 
-    val hiltVersion = "2.48"
+    val hiltVersion = "2.55"
     implementation("com.google.dagger:hilt-android:$hiltVersion")
     kapt("com.google.dagger:hilt-android-compiler:$hiltVersion")
 
--- app/src/main/java/com/example/appwithconfigurationbydaggerscopes/di/entry_point/LoggedInUserEntryPoint.kt
+++ app/src/main/java/com/example/appwithconfigurationbydaggerscopes/di/entry_point/LoggedInUserEntryPoint.kt
@@ -1,18 +1,10 @@
 package com.example.appwithconfigurationbydaggerscopes.di.entry_point
 
-import android.util.Log
 import com.example.appwithconfigurationbydaggerscopes.data.repository.ActiveMemoryRepositoryImpl
 import com.example.appwithconfigurationbydaggerscopes.data.repository.MemoryRepositoryImpl
 import com.example.appwithconfigurationbydaggerscopes.di.components.LoggedInUserComponent
-import com.example.appwithconfigurationbydaggerscopes.di.managers.LoggedInUserComponentManager
-import com.example.appwithconfigurationbydaggerscopes.domain.repository.ActiveMemoryRepository
-import com.example.appwithconfigurationbydaggerscopes.domain.repository.MemoryRepository
-import dagger.Module
-import dagger.Provides
 import dagger.hilt.EntryPoint
-import dagger.hilt.EntryPoints
 import dagger.hilt.InstallIn
-import dagger.hilt.components.SingletonComponent
 
 @EntryPoint
 @InstallIn(LoggedInUserComponent::class)
@@ -29,38 +21,3 @@ interface MemoryEntryPoint {
     fun getMemoryRepository(): MemoryRepositoryImpl
 }
 
-@Module
-@InstallIn(SingletonComponent::class)
-internal object RepositoryEntryPointsBridgeModule {
-    @Provides
-    internal fun provideMemoryRepository(
-        loggedInUserComponentManager: LoggedInUserComponentManager
-    ): MemoryRepository {
-        Log.w(
-            "RepositoryEntryPointsBridgeModule",
-            "obtaining memory repository from entrypoints"
-        )
-        return EntryPoints
-            .get(
-                loggedInUserComponentManager.loggedInUserComponent,
-                MemoryEntryPoint::class.java
-            )
-            .getMemoryRepository()
-    }
-
-    @Provides
-    internal fun provideActiveMemoryRepository(
-        loggedInUserComponentManager: LoggedInUserComponentManager
-    ): ActiveMemoryRepository {
-        Log.w(
-            "RepositoryEntryPointsBridgeModule",
-            "obtaining active memory repository from entrypoints"
-        )
-        return EntryPoints
-            .get(
-                loggedInUserComponentManager.loggedInUserComponent,
-                ActiveMemoryEntryPoint::class.java
-            )
-            .getActiveMemoryRepository()
-    }
-}
